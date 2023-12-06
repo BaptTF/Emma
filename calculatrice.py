525b5a5e1beefe149948f2ac51ee1374dd912983
JULLIEN Baptiste <baptiste.jullien@telecomnancy.eu>
2023-12-06 23:43:12 +0100

    Add the divide function with the / operator might have a problem with the division by zero

--- calculatrice.py
+++ calculatrice.py
@@ -1,5 +1,6 @@
 from add import add
 from multiply import mul
+from divide import divide
 
 def calcul(a, b):
     print("hello titiste")
@@ -9,6 +10,9 @@ def calcul(a, b):
 def added(a, b):
     first = a
     return add(first, b)
+
+def divided(a, b):
+    return divide(a, b)
 
 #ce que je veux 
 #je t'aime titiste
